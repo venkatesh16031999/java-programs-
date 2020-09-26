6ac333469c6285723612b4bfb76b407026763e0e
Venkatesh R <rvenki666@gmail.com>
2020-09-26 15:04:38 +0530

    Create combination.java

---
+++ permutations/combination.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		String arr[]={"a","b","c"};
+		ArrayList<String> list=new ArrayList<>();
+		per(arr,list,0);
+	}
+	
+	public static void per(String arr[],ArrayList<String> list,int index){
+	    
+	    if(list.size()>=arr.length){
+	        System.out.println(list);
+	        return;
+	    }
+	    
+	    for(int i=index;i<arr.length;i++){
+	        list.add(arr[i]);
+	        per(arr,list,i);
+	        list.remove(list.size()-1);
+	    }
+	    
+	}
+}
